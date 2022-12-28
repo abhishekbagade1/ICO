fed0464d7bbaf0fe660b9097e095658f3f215f57
abhishekbagade1 <abhishekbagade8@gmail.com>
2022-12-28 23:25:28 +0530

    new files added

---
+++ smart_contract/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x52ee5F09F00F190806d07dcD8Ad9996Cc1cAd610";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
